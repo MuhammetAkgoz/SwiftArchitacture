8316418555563fafd4aa0c77ac24cd6d2769c97d
MuhammetAkgöz <muhammet.akgz@gmail.com>
2025-02-04 20:36:20 +0300

    Initial Commit

---
+++ SwiftArchitacture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArchitacture
+//
+//  Created by Muhammet Akgöz on 4.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
